23333e058a1640123188f6708e41fd20efa63dd9
Osiven <venatiusokolo@gmail.com>
2022-09-08 22:04:21 -0700

    no. 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
-int main()
+int main(void)
 {
+	//string initialization
 	char phr[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(phr);
+	puts(phr); //writing string to stdout
 
 	return 0;
 }
