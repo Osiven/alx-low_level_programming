f19fdd0a26102ad40f90c442f9006c147e22254d
Osiven <venatiusokolo@gmail.com>
2022-09-12 16:03:27 -0700

    Using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/**
+ * main - print text using putchar
+ *
+ * Return: print text
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+	double ch == "and that piece of art is useful\" - Dora korpar, 2015-10-19";
+
+	putchar(ch);
+	return (0);
+}
