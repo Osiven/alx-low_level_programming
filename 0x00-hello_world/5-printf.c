5e88674a9b8b95e370d7a719c7557960993a2e18
Osiven <venatiusokolo@gmail.com>
2022-09-12 08:34:52 -0700

    Using "printf"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art
+ *
+ * Return: print texts
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
