e673ddf78204d66a51df7f02b21a08b6b325bdf3
Osiven <venatiusokolo@gmail.com>
2022-09-13 03:49:14 -0700

    Using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,10 @@
  * Return: print text
  */
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-	char ch;
-	double ch == "and that piece of art is useful\" - Dora korpar, 2015-10-19";
-
-	putchar(ch);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
