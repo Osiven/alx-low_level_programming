fe0e5c137c5580c5da05ebbdc521d48daa60a0d0
Osiven <venatiusokolo@gmail.com>
2022-09-14 21:37:23 -0700

    write text with putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+/**
+ * main - to print using putchar
+ *
+ * Return: Succesful program
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int pt;
+
+	pt = '_';
+	putchar(pt);
+	pt = 'p';
+	putchar(pt);
+	pt = 'u';
+	putchar(pt);
+	pt = 't';
+	putchar(pt);
+	pt = 'c';
+	putchar(pt);
+	pt = 'h';
+	putchar(pt);
+	pt = 'a';
+	putchar(pt);
+	pt = 'r';
+	putchar(pt);
+
+	return (0);
+}
