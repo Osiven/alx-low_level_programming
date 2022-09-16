96ffe0bffe01fbc63753915c01c01d721558ddbc
Osiven <venatiusokolo@gmail.com>
2022-09-16 13:57:11 -0700

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,13 @@
+#include <ctype.h>
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - to find uppercase letters
+ * @c: character
+ * Return: 0 or 1
+ */
+
 int _isupper(int c)
 {
 	if (isupper(c))
@@ -9,5 +18,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-
 }
