b7c5be67a5083493323e90215a87715d59c62300
Osiven <venatiusokolo@gmail.com>
2022-09-16 22:00:18 -0700

    To multiply integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,12 +6,12 @@
  * mul - to multiply two integers
  * @a: first integer
  * @b: second integer
- * Result: print product
+ * Return: print product
  */
 
 int mul(int a, int b)
 {
 	int product = a * b;
 
-	return product;
+	return (product);
 }
