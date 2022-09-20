2e2b523dc60f662f8be00231193ef4ef9e82a52c
Osiven <venatiusokolo@gmail.com>
2022-09-20 16:41:53 -0700

    Strlen measures strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - calculate the lenght of a string
+ * @*s: pointer
+ *
+ * Return: always 0 .
+ */
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+int _strlen(char *s)
+{
+	strlen(*s)
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-void swap_int(int *a, int*b);
+int _strlen(char *s);
 
 #endif
