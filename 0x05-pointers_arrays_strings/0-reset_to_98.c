5b5e52cf85ac6787da6f5ffa5d183924d775fa3f
Osiven <venatiusokolo@gmail.com>
2022-09-19 21:23:29 -0700

    Reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - to update the value of an int
+ * @n: a parameter
+ *
+ * Return: Always zero
+ */
+#include <stdio.h>
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	n* = 402;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The use of Pointers, arrays and strings
