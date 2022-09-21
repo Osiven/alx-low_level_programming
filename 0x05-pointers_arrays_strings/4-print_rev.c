ca48ebcf6228bc16853eac72a870ce74d6c7e87c
Osiven <venatiusokolo@gmail.com>
2022-09-21 06:00:21 -0700

    The reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,13 +17,13 @@ char *strrev(char *str)
 {
 	char *p1, *p2;
 
-	if (! str || ! *str)
-		return str;
+	if (!str || !*str)
+		return (str);
 	for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
 	{
 		*p1 ^= *p2;
 		*p2 ^= *p1;
 		*p1 ^= *p2;
 	}
-	return str;
+	return (str);
 }
