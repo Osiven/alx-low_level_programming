305fccfa0225d26b7d4694a28b62315fd2f567fa
Osiven <venatiusokolo@gmail.com>
2022-09-21 05:21:31 -0700

    The reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+/**
+ * print_rev - Print string in reverse
+ * @s: the name of the string array
+ *
+ * Return: Always zero.
+ */
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-void _puts(char *str);
+void print_rev(char *s);
 
 #endif
